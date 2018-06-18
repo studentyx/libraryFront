2c04ada71204db74a9bc7e9e8d9f87a224d47bb9
studentyx <tfmuniversity@protonmail.com>
2018-06-18 22:07:33 +0200

    BookDetails Edit/Save/Cancel Update

--- src/app/bookDetails/bookDetails.component.ts
+++ src/app/bookDetails/bookDetails.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { BookService } from '../shared/book.service';
 import { Book } from '../shared/book.model';
-import { ActivatedRoute } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
     templateUrl: 'bookDetails.component.html',
@@ -15,10 +15,19 @@ export class BookDetailsComponent implements OnInit {
     bookId: string;
     book: Book;
 
+    propertyString: string;
+
+    genreUpdate: string;
+    tagsUpdate: string;
+
     constructor(private bookService: BookService,
-        private route: ActivatedRoute) {
+        private router: Router, private route: ActivatedRoute) {
+
         this.route.params.subscribe(params => this.bookId = params['id']);
-        this.book = { title: "default", image: "default" }
+        this.book = { title: "default", image: "default" };
+        
+        this.propertyString = '';
+        
     }
 
     ngOnInit(): void {
@@ -28,7 +37,32 @@ export class BookDetailsComponent implements OnInit {
     synchronize() {
         this.bookService.read(this.bookId).subscribe(data => {
             this.book = data;
+            this.genreUpdate = this.book.genre.join( ", " );
+            this.tagsUpdate = this.book.tags.join( ", " );
         });
+    }
+
+    editProperty( propertyKey: string ){
+        this.propertyString = propertyKey;
+        console.log( this.propertyString );
+    }
+
+    cancelProperty(){
+        this.propertyString = '';
+        this.genreUpdate = this.book.genre.join( ", " );
+        this.tagsUpdate = this.book.tags.join( ", " );
+    }
+
+    saveProperty(){
+        this.propertyString = '';
+        this.book.genre = this.genreUpdate.split( ", " );
+        this.book.tags = this.tagsUpdate.split( ", " );
+
+        this.bookService.update(this.book).subscribe();
+    }
+
+    editPropertyActive( property: string ){
+        return this.propertyString === property;
     }
 
 }
